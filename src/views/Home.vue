b867138bab1558c61233b3f7ca2bdac4e367f029
huhn <sehe89@gmail.com>
2019-04-28 22:42:42 +0200

    add einfachiota logo and setup build ci

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="einfachiotatangle" src="../assets/einfachiotatangle.jpg">
+    <HelloWorld msg="Willkommen bei einfachIOTA"/>
   </div>
 </template>
 
