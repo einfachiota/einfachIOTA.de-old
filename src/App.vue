44091e7a698fbfe235c448f476c8dde91acf5f38
huhn <sehe89@gmail.com>
2019-05-04 07:59:25 +0200

    add nav

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,77 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/news">Neuigkeiten</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <NavBar />
     <router-view/>
   </div>
 </template>
 
+<script>
+import NavBar from '@/components/NavBar.vue'
+
+export default {
+    components: {NavBar},
+}
+</script>
+
 <style>
+
+@import url('https://fonts.googleapis.com/css?family=Nunito');
+
+body {
+ margin: 0;
+} 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Nunito', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+ 
+}
+h1, h2, h3, h4 {
+  margin: 0;
+  font-weight: 700;
+}
+h1 {
+  font-size: 62px;
+  line-height: 1.25;
+}
+h2 {
+  font-size: 42px;
+}
+h3 {
+  font-size: 30px;
+}
+h4 {
+  margin-bottom: 10px;
+  font-size: 24px;
+}
+p {
+  font-size: 16px;
+  line-height: 1.5;
+}
+.main {
+  margin-top: 70px;
+  padding-top: 24px;
+}
+:root {
+  --primary: #139B48;
+  --primary-2: #A2CF6F;
+  --secondary: #0B5D9E;
+  --secondary-2: #5AB0B2;
+  --dark: #081C1D;
+  --darker: #0F3535;
+  --light: #E2E2E2;
+  --white: #FFFFFF;
+  --white_5: rgba(255,255,255,.05);
+  --white_10: rgba(255,255,255,.1);
+  --white_25: rgba(255,255,255,.25);
+  --white_50: rgba(255,255,255,.5);
+  --red: #F16465;
+  --orange: #E69255;
+  --yellow: #E6BE54;
+  --purple: #8F60A8;
+  --border-radius: 10px;
+  --border-radius-sm: 5px;
+  --box-shadow: 0 3px 6px rgba(0,0,0,.15);
+  --transition-in-out: all 0.3s ease-in-out;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,127 @@
+<template>
+  <nav class="main-nav">
+
+
+    <router-link to="/" class="nav-item nav-item--logo" exact-active-class="active">
+        <img alt="einfachIOTA" class="logo" src="../assets/logo.svg" />
+    </router-link>
+
+
+    <div class="nav-menu">
+      <router-link to="/news" class="nav-item" active-class="active">
+        <span>Neuigkeiten</span>
+      </router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  computed: {
+    itemsInCart() {
+      let cart = this.$store.getters.cartProducts;
+      return cart.reduce((accum, item) => accum + item.quantity, 0);
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+// ------------------------------------------
+//  N A V I G A T I O N
+// ------------------------------------------
+.main-nav {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  height: 70px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: var(--white);
+  box-shadow: 0 3px 6px rgba(0,0,0,.15);
+  transition: all 200ms ease-in-out;
+  .nav-menu {
+    display: flex;
+    align-items: center;
+    height: 100%;
+  }
+  .nav-item {
+    position: relative;
+    overflow: hidden;
+    height: 100%;
+    padding: 0 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: bold;
+    color: var(--dark);
+    text-decoration: none;
+    opacity: .75;
+    transition: all 200ms ease-in-out;
+    &--logo {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding-left: 15px;
+      padding-right: 15px;
+      opacity: 1;
+    }
+    .logo {
+      width: 55px;
+
+    }
+    &:before {
+      content: '';
+      position: absolute;
+      bottom: -5px;
+      height: 10px;
+      width: 0;
+      border-radius: 10px;
+      transition: all 200ms ease-in-out;
+    }
+    &:hover, &.active {
+      opacity: 1;
+      background-color: rgba(0,0,0,.05);
+      color: var(--secondary-2);
+      &:before {
+        content: '';
+        position: absolute;
+        bottom: -5px;
+        height: 10px;
+        width: 50px;
+        border-radius: 10px;
+        background-color: var(--secondary-2);
+      }
+    }
+    &:hover {
+      cursor: pointer;
+      &:before {
+        width: 10px;
+      }
+    }
+  }
+  &.top {
+    background-color: rgba(0,0,0,.1);
+    box-shadow: none;
+    .nav-item {
+      color: var(--white);
+      &--logo {
+        .logo-wrapper {
+          filter: invert(0%);
+        }
+        .nile-typo {
+          filter: invert(0%);
+        }
+      }
+      &:hover, &.active {
+        background-color: rgba(255,255,255,.1);
+      }
+    }
+  }
+}
+
+</style>
