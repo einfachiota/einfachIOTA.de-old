fcf177e9832885d8c53f3b2487983670232dd0b8
huhn <sehe89@gmail.com>
2019-05-20 23:35:53 +0200

    remove link

--- src/views/Supporter.vue
+++ src/views/Supporter.vue
@@ -27,29 +27,23 @@
       </div>
     </div>
     <div class="container">
-      <p>Wir von einfachIOTA betrachten die IOTA Community als eine Art große Familie die zusammen hält und sich gegenseitig unterstützt. 
+      <p>
+        Wir von einfachIOTA betrachten die IOTA Community als eine Art große Familie die zusammen hält und sich gegenseitig unterstützt.
         Daher berichten wir jede Woche über ein neues, oder auch ein bereits bekanntes Projekt aus dem IOTA Ökosystem.
-        Wir hoffen es dadurch ein bisschen bekannter zu machen und freuen uns, wenn wir die Personen hinter den Projekten besser kennen lernen. 
-        Und wie es sich bei Freunden und Verwandten so gehört, wollen wir natürlich immer sofort sehen wie es ihnen aktuell ergeht und was sie so treiben. 
-        Wir hoffen, dass hier in Zukunft noch viele weitere Freunde und Verwandte hinzu kommen! 🤗</p>
+        Wir hoffen es dadurch ein bisschen bekannter zu machen und freuen uns, wenn wir die Personen hinter den Projekten besser kennen lernen.
+        Und wie es sich bei Freunden und Verwandten so gehört, wollen wir natürlich immer sofort sehen wie es ihnen aktuell ergeht und was sie so treiben.
+        Wir hoffen, dass hier in Zukunft noch viele weitere Freunde und Verwandte hinzu kommen! 🤗
+      </p>
       <table class="el-table hover">
         <tr>
-          <th>Name/Webseite</th>
+          <th>Projekt</th>
           <th>Kurzbeschreibung</th>
-          <th>Projekt der Woche</th>
         </tr>
         <tr>
           <td>
             <a target="_blank" rel="noopener noreferrer" href="https://powsrv.io/">powsrv.io</a>
           </td>
           <td>POW-as-a-Service-Anbieter für IOTA</td>
-          <td>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.einfachiota.de/projekt-der-woche-powsrv-io/"
-            >Link</a>
-          </td>
         </tr>
         <tr>
           <td>
@@ -60,26 +54,12 @@
             >Piri</a>
           </td>
           <td>Proxy und Load Balancer für IOTA Nodes</td>
-          <td>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.einfachiota.de/projekt-der-woche-piri/"
-            >Link</a>
-          </td>
         </tr>
         <tr>
           <td>
             <a target="_blank" rel="noopener noreferrer" href="http://qubiclite.org/">Qubic Lite</a>
           </td>
           <td>QLite Ecosystem, ein Prototyp von Qubic von microhash</td>
-          <td>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.einfachiota.de/projekt-der-woche-qubic-lite/"
-            >Link</a>
-          </td>
         </tr>
         <tr>
           <td>
@@ -90,13 +70,6 @@
             >IOTA Address Notifier</a>
           </td>
           <td>IOTA Address Notifier (Telegram)</td>
-          <td>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.einfachiota.de/projekt-der-woche-iota-address-notifie/"
-            >Link</a>
-          </td>
         </tr>
         <tr>
           <td>
@@ -107,13 +80,6 @@
             >ioTipBot</a>
           </td>
           <td>IOTA TipBot für verschiedene Chatplattformen</td>
-          <td>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.einfachiota.de/projekt-der-woche-iotipbot/"
-            >Link</a>
-          </td>
         </tr>
         <tr>
           <td>
@@ -124,39 +90,18 @@
             >Node-Red-MAM</a>
           </td>
           <td>Entwicklungsumgebung und visueller Editor für das IOT</td>
-          <td>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.einfachiota.de/projekt-der-woche-node-red-mam/"
-            >Link</a>
-          </td>
         </tr>
         <tr>
           <td>
             <a target="_blank" rel="noopener noreferrer" href="https://iota.partners/">IOTA.partners</a>
           </td>
           <td>interaktive Anleitung zum Installieren einer IOTA Node</td>
-          <td>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.einfachiota.de/projekt-der-woche-iota-partners/"
-            >Link</a>
-          </td>
         </tr>
         <tr>
           <td>
             <a target="_blank" rel="noopener noreferrer" href="https://lidbot.com/">TWO</a>
           </td>
           <td>IoT Müll Management (Umgenannt in 'lidbot')</td>
-          <td>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.einfachiota.de/projekt-der-woche-two-iot-waste-management/"
-            >Link</a>
-          </td>
         </tr>
         <tr>
           <td>
@@ -167,52 +112,24 @@
             >Tanglenodes.io</a>
           </td>
           <td>Online Mietservice für IOTA Fullnodes</td>
-          <td>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.einfachiota.de/projekt-der-woche-tanglenodes-io/"
-            >Link</a>
-          </td>
         </tr>
         <tr>
           <td>
             <a target="_blank" rel="noopener noreferrer" href="http://publiciota.com/">publicIOTA</a>
           </td>
           <td>Unabhängige Gruppe von IOTA Enthusiasten</td>
-          <td>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.einfachiota.de/projekt-der-woche-publiciota/"
-            >Link</a>
-          </td>
         </tr>
         <tr>
           <td>
             <a target="_blank" rel="noopener noreferrer" href="https://thetangle.org/">TheTangle.org</a>
           </td>
           <td>Dienst zum Durchsuchen des IOTA-Tangles</td>
-          <td>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.einfachiota.de/projekt-der-woche-thetangle-org/"
-            >Link</a>
-          </td>
         </tr>
         <tr>
           <td>
             <a target="_blank" rel="noopener noreferrer" href="https://pact.care/">Untangle Care</a>
           </td>
           <td>IOTA im Gesundheitswesen: Daten gehören dem Patient</td>
-          <td>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.einfachiota.de/projekt-der-woche-untangle-care/"
-            >Link</a>
-          </td>
         </tr>
         <tr>
           <td>
@@ -223,13 +140,6 @@
             >TangleSheep</a>
           </td>
           <td>Echte Schaafe automatisch füttern durch IOTA</td>
-          <td>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.einfachiota.de/projekt-der-woche-tanglesheep/"
-            >Link</a>
-          </td>
         </tr>
         <tr>
           <td>
@@ -240,13 +150,6 @@
             >TangleID (BiiLabs)</a>
           </td>
           <td>Open-Source Projekt von BiiLabs (DLT-Incubator aus Taiwan)</td>
-          <td>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.einfachiota.de/projekt-der-woche-tangleid-biilabs/"
-            >Link</a>
-          </td>
         </tr>
         <tr>
           <td>
@@ -257,26 +160,12 @@
             >IOTA controlled agent</a>
           </td>
           <td>Leichtgewichtiger Node für das finale IOTA Protokoll</td>
-          <td>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.einfachiota.de/projekt-der-woche-iota-controlled-agent/"
-            >Link</a>
-          </td>
         </tr>
         <tr>
           <td>
             <a target="_blank" rel="noopener noreferrer" href="https://www.iota-talk.de/">IOTA Talk</a>
           </td>
           <td>Forum für IOTA Community</td>
-          <td>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.einfachiota.de/projekt-der-woche-iota-talk/"
-            >Link</a>
-          </td>
         </tr>
         <tr>
           <td>
@@ -287,13 +176,6 @@
             >The Tangle Bay</a>
           </td>
           <td>Landkarte für Projekten und Geschäften mit IOTA-Bezug</td>
-          <td>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.einfachiota.de/projekt-der-woche-the-tangle-bay/"
-            >Link</a>
-          </td>
         </tr>
         <tr>
           <td>
@@ -304,13 +186,6 @@
             >Think Tangle</a>
           </td>
           <td>Sammlung aller Ideen, Konzepte, POC, usw. rund um IOTA</td>
-          <td>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.einfachiota.de/projekt-der-woche-thinktangle/"
-            >Link</a>
-          </td>
         </tr>
         <tr>
           <td>
@@ -321,13 +196,6 @@
             >IOTAlink.directory</a>
           </td>
           <td>Webseite mit nahezu allen wichtigen IOTA Links</td>
-          <td>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.einfachiota.de/projekt-der-woche-iotalink-directory/"
-            >Link</a>
-          </td>
         </tr>
         <tr>
           <td>
@@ -338,26 +206,12 @@
             >IOTA Einsteiger Guide</a>
           </td>
           <td>Einsteiger Guide für IOTA: Word Datei mit fast 100 Seiten</td>
-          <td>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.einfachiota.de/projekt-der-woche-iota-einsteiger-guide/"
-            >Link</a>
-          </td>
         </tr>
         <tr>
           <td>
             <a target="_blank" rel="noopener noreferrer" href="https://xdk2mam.io/">XDK2MAM</a>
           </td>
           <td>Open-Source-Code zu schreiben für den Bosch XDK110 Sensor</td>
-          <td>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.einfachiota.de/projekt-der-woche-xdk2mam/"
-            >Link</a>
-          </td>
         </tr>
         <tr>
           <td>
@@ -368,13 +222,6 @@
             >IOTA Coordinator Monitor</a>
           </td>
           <td>Webseite mit grafischen Darstellungen des IOTA Coordinators</td>
-          <td>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.einfachiota.de/projekt-der-woche-iota-coordinator-monitor/"
-            >Link</a>
-          </td>
         </tr>
         <tr>
           <td>
@@ -385,13 +232,6 @@
             >ProductID</a>
           </td>
           <td>Der digitale Zwillinge für Produkte</td>
-          <td>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.einfachiota.de/projekt-der-woche-productid/"
-            >Link</a>
-          </td>
         </tr>
         <tr>
           <td>
@@ -402,30 +242,12 @@
             >Hello IOTA</a>
           </td>
           <td>Webseite mit interessanten Artikeln und einem Forum.</td>
-          <td>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.einfachiota.de/projekt-der-woche-hello-iota/"
-            >Link</a>
-          </td>
         </tr>
         <tr>
           <td>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://tanglebay.org"
-            >Tangle Bay</a>
+            <a target="_blank" rel="noopener noreferrer" href="https://tanglebay.org">Tangle Bay</a>
           </td>
           <td>Community Plattform - multilinguales Forum</td>
-          <td>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://tanglebay.org"
-            >Link</a>
-          </td>
         </tr>
       </table>
       <div class="divider"></div>
