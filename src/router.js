84e8b78c91a95cb3cc09a52af06fe34b925e3ae9
huhn <sehe89@gmail.com>
2019-05-22 22:30:10 +0200

    fixed routing

--- src/router.js
+++ src/router.js
@@ -14,6 +14,13 @@ import Supporter from './views/Supporter.vue'
 Vue.use(Router)
 
 export default new Router({
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  },
   routes: [
     {
       path: '/',
