31fdc386a8943ecd99e59c4431d7049f7d76f4ce
huhn <sehe89@gmail.com>
2019-05-30 03:17:15 +0200

    fix image size

--- src/views/CoordicidePage.vue
+++ src/views/CoordicidePage.vue
@@ -142,15 +142,20 @@ export default {
     display: flex;
     justify-content: center;
   }
+  img {
+    max-width: 100%;
+    max-height: 100%;
+
+    border: 2px solid var(--light);
+    border-radius: 10px;
+    margin-bottom: 10px;
+  }
   img.kg-image {
     display: block;
     width: 100%;
     height: 100%;
     background-position: center center;
     background-size: cover;
-    border: 2px solid var(--light);
-    border-radius: 10px;
-    margin-bottom: 10px;
   }
   figcaption {
     font-size: 12px;
